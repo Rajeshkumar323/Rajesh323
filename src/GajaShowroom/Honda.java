83eeb152562e7285b91715650bde046843b1e546
Rajesh323 <rajeshraju00051@gmail.com>
2021-12-31 16:23:32 +0530

    Folder structure

---
+++ src/GajaShowroom/Honda.java
@@ -0,0 +1,17 @@
+package GajaShowroom;
+
+public class Honda extends ReModel{
+public void hondaactiva() {
+System.out.println("blackcolor");
+}
+public void hondaactiva2() {
+System.out.println("bluecolor");
+}
+public static void main(String[] args) {
+ Honda h=new Honda();
+ h.hondaactiva();
+ h.hondaactiva2();
+ h.re2018modelclassic350cc();
+ h.re2020modelclassic350cc();
+}
+}
---
+++ src/GajaShowroom/ReModel.java
@@ -0,0 +1,12 @@
+package GajaShowroom;
+
+public class ReModel {
+public void re2018modelclassic350cc() {
+System.out.println("1.95 lakhs");
+}	
+public void re2020modelclassic350cc() {
+System.out.println("2.15 lakhs");
+}
+
+}
+
